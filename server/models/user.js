1fcb9c574df0ad1ea8a632ac4f4afb7b1b024636
Miloš Kovačević <miloscpt@gmail.com>
2018-01-31 22:56:19 +0100

    shit

--- server/models/user.js
+++ server/models/user.js
@@ -1,12 +1,33 @@
-let mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const validator = require('validator');
 
 let User = mongoose.model('User', {
     email: {
         required: true,
         trim: true,
         type: String,
-        minlength: 3
-    }
+        minlength: 3,
+        unique: true,
+        validate: {
+            validator: validator.isEmail,
+            message: '{VALUE} is not a valid email!'
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6
+    },
+    tokens: [{
+        access: {
+            type: String,
+            required: true
+        },
+        token: {
+            type: String,
+            required: true
+        }
+    }]
 });
 
 module.exports = { User };
