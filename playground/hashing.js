6438a897efef7c0280ea1f9982a0dd0570bacadd
Miloš Kovačević <miloscpt@gmail.com>
2018-02-01 22:44:16 +0100

    signup done

---
+++ playground/hashing.js
@@ -0,0 +1,41 @@
+const { SHA256 } = require('crypto-js');
+const jwt = require('jsonwebtoken');
+
+let data = {
+    id: 10
+};
+
+let token = jwt.sign(data, '123abc');
+
+console.log(token);
+
+let decoded = jwt.verify(token, '123abc');
+console.log(`decoded: ${decoded}`);
+console.log(decoded)
+// let message = 'I am user number 3 ';
+// let hash = SHA256(message).toString();
+
+// console.log(`Message: ${message}`);
+// console.log(`Hashed message: ${hash}`);
+
+
+// let data = {
+//     id: 4
+// };
+
+// let token = {
+//     data,
+//     hash: SHA256(JSON.stringify(data) + 'somesecret').toString()
+// };
+
+// let resultHash = SHA256(JSON.stringify(token.data) + 'somesecret').toString();
+
+// token.data.id = 5;
+// token.hash = SHA256(JSON.stringify(token.data)).toString();
+
+// if (resultHash === token.hash) {
+//     console.log('data was not changed!');
+// } else 
+// {
+//     console.log('data was changed!');
+// }
--- server/models/user.js
+++ server/models/user.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
 
-let User = mongoose.model('User', {
+let UserSchema = new mongoose.Schema({
     email: {
         required: true,
         trim: true,
@@ -29,5 +31,32 @@ let User = mongoose.model('User', {
         }
     }]
 });
+
+UserSchema.methods.toJSON = function() {
+    let user = this;
+    let userObject = user.toObject();
+
+    return _.pick(userObject,['_id','email']);
+}
+
+UserSchema.methods.generateAuthToken = function() {
+    let user = this;
+    let access = 'auth';
+    let token =  jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
+
+    user.tokens.push({
+        access,
+        token
+    });
+
+    return user.save().then(() => {
+        return token;
+    })
+};
+
+
+
+
+let User = mongoose.model('User', UserSchema);
 
 module.exports = { User };
