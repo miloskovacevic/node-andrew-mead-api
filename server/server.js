a5a9b9581f4f378e1a176ceb37bdafbedb7f1f3a
Miloš Kovačević <miloscpt@gmail.com>
2018-02-04 22:43:14 +0100

    authenticate routes

--- server/server.js
+++ server/server.js
@@ -42,10 +42,11 @@ app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user);
 });
 
-app.post('/todos', (req, res) => {
+app.post('/todos', authenticate, (req, res) => {
     console.log(req.body);
     let todo = new Todo({
-        text: req.body.text
+        text: req.body.text,
+        _creator: req.user._id
     })
     todo.save()
         .then((doc) => {
@@ -56,20 +57,25 @@ app.post('/todos', (req, res) => {
 
 });
 
-app.get('/todos', (req, res) => {
-    Todo.find().then((todos) => {
+app.get('/todos', authenticate, (req, res) => {
+    Todo.find({
+        _creator: req.user._id
+    }).then((todos) => {
         res.send({todos});
     }, (err) => {
         res.status(400).send(e);
     })
 });
 
-app.get('/todos/:id', (req, res) => {
+app.get('/todos/:id', authenticate, (req, res) => {
     let id = req.params.id;
     if (!ObjectId.isValid(id)) {
         res.status(404).send();
     }
-    Todo.findById(id)
+    Todo.findOne({
+        _id: id,
+        _creator: req.user._id
+    })
         .then((todo) => {
             if (!todo) 
                 return res.status(404).send();
@@ -80,14 +86,17 @@ app.get('/todos/:id', (req, res) => {
 })
 
 
-app.delete('/todos/:id', (req,res) => {
+app.delete('/todos/:id', authenticate, (req,res) => {
     let id = req.params.id;
 
     if (!ObjectId.isValid(id)) {
         res.status(404).send();
     }
 
-    Todo.findByIdAndRemove(id).then((todo) => {
+    Todo.findOneAndRemove({
+        _id: id,
+        _creator: req.user._id
+    }).then((todo) => {
         if (!todo)
             return res.status(404).send();
         res.status(200).send({todo});
@@ -97,7 +106,7 @@ app.delete('/todos/:id', (req,res) => {
 })
 
 
-app.patch('/todos/:id', (req, res) => {
+app.patch('/todos/:id', authenticate, (req, res) => {
     let id = req.params.id;
     let body = _.pick(req.body, ['text','completed']);
 
@@ -107,13 +116,15 @@ app.patch('/todos/:id', (req, res) => {
 
     if (_.isBoolean(body.completed) && body.completed) {
         body.completedAt = new Date().getTime();
-
     } else {
         body.completed = false;
         body.completedAt = null;
     }
 
-    Todo.findByIdAndUpdate(id, {
+    Todo.findOneAndUpdate({
+        _id: id,
+        _creator: req.user._id
+    }, {
         $set: body
     }, {new : true}).then((todo) => {
         if (!todo)
@@ -140,6 +151,16 @@ app.post('/users/login', (req, res) => {
     }).catch((err) => {
         res.status(400).send();
     })
+});
+
+app.delete('/users/me/token', authenticate, (req, res) => {
+    req.user.removeToken(req.token).then(() => {
+        res.status(200).send();
+    }, () => {
+        res.status(400).send();
+    });
+
+
 });
 
 
